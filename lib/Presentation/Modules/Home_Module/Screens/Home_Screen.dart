6a89902dc0fc3c12ff7467d92da9c532a19c8d04
MoEhsan10 <mohamedehsan391@gmail.com>
2024-10-13 03:55:37 +0300

    + Tashbeh-Tab

--- lib/Presentation/Modules/Home_Module/Screens/Home_Screen.dart
+++ lib/Presentation/Modules/Home_Module/Screens/Home_Screen.dart
@@ -18,7 +18,7 @@ class _HomeScreenState extends State<HomeScreen> {
   List<Widget> tabs = [
     QuranTab(),
     HadithTab(),
-    Tasbeh_Tab(),
+    TasbehTab(),
     RadioTab(),
     SettingsTab(),
   ];
